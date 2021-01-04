f46d07b33f569ff2d823e43f80a3eebacc2150aa
Sayantan Roy <uchihaitachi0799@gmail.com>
2021-01-04 16:40:08 +0530

    Initial project grid layout ready.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="grid-container">
+        <div class="search-container">Search Container</div>
+        <div class="cities-container">Cities Container</div>
+        <div class="activities-container">Activities Container</div>  
+      </div>
     </div>
   );
 }
